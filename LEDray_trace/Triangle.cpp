7860434e307aa82f18f0585cc6bf5eb84220ba88
FireDrake987 <danielmlouk@gmail.com>
2026-03-27 09:11:48 -0500

    Changed triangle intersection implementation to use barycentric coordinates

--- LEDray_trace/Triangle.cpp
+++ LEDray_trace/Triangle.cpp
@@ -9,20 +9,31 @@ Triangle::Triangle() : Plane() {
 	this->p1 = this->p2 = this->p3 = Point3D();
 }
 intersectionInfoStruct Triangle::getIntersection(Ray ray) {
-	intersectionInfoStruct info = Plane::getIntersection(ray);
-	if (info.t < 0) {
-		return info;//No intersection with plane, so no intersection with triangle
-	}
-	Point3D p = info.point;
-	double sub1 = triangleArea(p, p1, p2);
-	double sub2 = triangleArea(p, p2, p3);
-	double sub3 = triangleArea(p, p1, p3);
-	double area = triangleArea(p1, p2, p3);
-	if(sub1 + sub2 + sub3 > area * 1.001) {
-		info.t = -1;//Point outside triangle
-		return info;
-	}
-	return info;//Point inside triangle, we are fine
+    intersectionInfoStruct info = Plane::getIntersection(ray);
+    if (info.t < 0) return info;
+
+    Vector v0(p1, p2);
+    Vector v1(p1, p3);
+    Vector v2(p1, info.point);
+
+    double d00 = v0.dot(v0);
+    double d01 = v0.dot(v1);
+    double d11 = v1.dot(v1);
+    double d20 = v2.dot(v0);
+    double d21 = v2.dot(v1);
+
+    double denom = d00 * d11 - d01 * d01;
+    if (fabs(denom) < 1e-12) { info.t = -1; return info; }
+
+    double v = (d11 * d20 - d01 * d21) / denom;
+    double w = (d00 * d21 - d01 * d20) / denom;
+    double u = 1.0 - v - w;
+
+    const double epsilon = -1e-6;
+    if(u < epsilon || v < epsilon || w < epsilon) {
+        info.t = -1;
+    }
+    return info;
 }
 double Triangle::triangleArea(Point3D p1, Point3D p2, Point3D p3) {
 	Vector v1(p1, p2);
