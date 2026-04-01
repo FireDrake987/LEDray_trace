4e1bd8b45ab99972b710300a862c104257ba0b59
FireDrake987 <danielmlouk@gmail.com>
2026-04-01 07:53:33 -0500

    Bugfixed Plane constructor

--- LEDray_trace/Plane.cpp
+++ LEDray_trace/Plane.cpp
@@ -5,9 +5,9 @@
 
 Plane::Plane(Material material, Point3D p1, Point3D p2, Point3D p3) {
 	this->mat = material;
-	Vector v1 = Vector(p1, p2);
-	Vector v2 = Vector(p1, p3);
-	Vector n = v2.cross(v1).normalize();
+	Vector v1 = Vector(p2, p1);
+	Vector v2 = Vector(p3, p1);
+	Vector n = v1.cross(v2).normalize();
 	this->A = n.getX();
 	this->B = n.getY();
 	this->C = n.getZ();
